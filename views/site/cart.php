a025595432a6a2281124999118d92aa0b9cda168
uzaifahamdan <uzaifahamdan@gmail.com>
2021-11-18 10:44:29 +0300

    first commit

---
+++ views/site/cart.php
@@ -0,0 +1,21 @@
+<?php
+@var array $items
+
+?>
+
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>Product</th>
+            <th>Image</th>
+            <th>Unit Price</th>
+            <th>Quantity</th>
+            <th>Total Price</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+    
+    </tbody>
+
+</table>
